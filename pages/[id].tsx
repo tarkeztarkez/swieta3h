7e0e068e26367abd0524661c09f66e5329b82278
Marcin Szyda <szyda47@gmail.com>
2021-11-29 21:21:53 +0100

    działa

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import { useRouter } from "next/dist/client/router";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import YouTube from "../components/Youtube";
 import { getYoutubeId } from "../lib/getYoutubeId";
@@ -12,20 +12,37 @@ const Home: NextPage = () => {
   const [url, seturl] = useState("");
   const [text, settext] = useState("");
   const router = useRouter();
+  const querryId = router.query.id;
 
   useEffect(() => {
+    if (!querryId) {
+      return;
+    }
     (async () => {
-      const data = await (
-        await fetch(`https://api.npoint.io/add70bdaf588eb3cb38c/`)
-      ).json();
+      const data: { days: Array<{ url: string; id: string; text: string }> } =
+        await (
+          await fetch(`https://api.npoint.io/add70bdaf588eb3cb38c/`)
+        ).json();
+
+      if (data) {
+        console.log(data);
+        const record = data.days.find(({ id }) => id == querryId);
+        console.log(record);
+        console.log(router.query.id);
+        if (record) {
+          seturl(record.url);
+          console.log(url);
+        }
+        setloading(false);
+      }
     })();
-  }, []);
+  }, [querryId]);
   return (
     <div>
-      {loading ?? (
+      {!loading && (
         <div className={styles.container}>
-          {text ?? <h2>{text}</h2>}
-          {url ?? <YouTube url={url} />}
+          {text && <h2>{text}</h2>}
+          {url && <YouTube url={url} />}
         </div>
       )}
     </div>
