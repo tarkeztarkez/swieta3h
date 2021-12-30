c554865865e87c8d5a5e8f080b99acabb297cb13
Marcin Szyda <szyda47@gmail.com>
2021-12-30 20:51:15 +0100

    jest kurwa responsywnosc

--- components/Youtube.tsx
+++ components/Youtube.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
-import ReactPlayer, { ReactPlayerProps } from 'react-player';
-
+import React, {
+  CSSProperties,
+  useEffect,
+  useLayoutEffect,
+  useState,
+} from "react";
+import ReactPlayer, { ReactPlayerProps } from "react-player";
 
 function Youtube(props: ReactPlayerProps) {
-    return <ReactPlayer {...props} />
+  const [size, setSize] = useState([0, 0]);
+  const [width, setwidth] = useState(640);
+  const [height, setheight] = useState(360);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+
+  useEffect(() => {
+    if (window.innerHeight < 390) {
+      setheight(window.innerHeight - 10);
+      setwidth(Math.floor(height * 1.777));
+    } else if (window.innerWidth < 670) {
+      setwidth(window.innerWidth - 10);
+      setheight(Math.floor(width * 0.5625));
+    } else {
+      setwidth(640);
+      setheight(360);
+    }
+  }, [size]);
+
+  return <ReactPlayer {...props} width={width} height={height} />;
 }
 
-export default Youtube
+export default Youtube;
